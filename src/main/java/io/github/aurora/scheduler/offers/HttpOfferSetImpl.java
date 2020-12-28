7bd3857f27cde199046fe9d4b25463937404842d
Renan DelValle <commit@ridv.xyz>
2020-12-28 10:41:25 -0800

    Removes wildcard import and replaces it with classes needed.

--- src/main/java/io/github/aurora/scheduler/offers/HttpOfferSetImpl.java
+++ src/main/java/io/github/aurora/scheduler/offers/HttpOfferSetImpl.java
@@ -18,7 +18,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentSkipListSet;
 import java.util.stream.Collectors;
 
